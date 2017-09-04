73154edd9871029f849a10723130ad34397496f2
Mark Wylde <me@markwylde.co.uk>
2017-09-04 21:28:47 +0100

    Automatically find layouts

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
 const fs = require('fs')
+const path = require('path')
 const ejs = require('ejs')
 
-module.exports = function (layouts) {
-  return function (filePath, options, callback) {
-    fs.readFile(filePath, function (err, data) {
-      if (err) throw err
-      data = ejs.render(data.toString(), options)
+const rootDir = path.join(path.dirname(require.main.filename), 'layouts')
 
-      let vars = {}
+module.exports = function (filePath, options, callback) {
+  fs.readFile(filePath, function (err, data) {
+    if (err) throw err
+    data = ejs.render(data.toString(), options)
 
-      const varsRx = new RegExp(`<vars ([\\s\\S]*?)(><\\/vars>|\\/>)`, 'g')
-      data.replace(varsRx, function (all, items) {
-        items.replace(/(.*?)=(.*?)( |$|\n)/g, function (all, key, val) {
-          if (val.substr(-1) === '"') {
-            vars[key.trim()] = val.trim().slice(1, -1)
-          } else {
-            vars[key.trim()] = val.trim()
-          }
-        })
-      })
+    let vars = {}
 
-      let injects = {
-        leftovers: [data]
-      }
-
-      const insertRx = new RegExp(`<insert into="(.*?)">([\\s\\S]*?)</insert>`, 'g')
-      data.replace(insertRx, function (all, where, content) {
-        if (!injects[where]) injects[where] = []
-        injects[where].push(content)
-        injects.leftovers[0] = injects.leftovers[0].replace(all, '')
-      })
-
-      for (let key in injects) {
-        injects[key] = injects[key].join('\n')
-      }
-
-      options = Object.assign(options, vars, injects)
-
-      fs.readFile(layouts[options.template || 'default'], function (err, data) {
-        if (err) console.log(err)
-        const finalHtml = ejs.render(data.toString(), options)
-
-        callback(null, finalHtml)
+    const varsRx = new RegExp(`<vars ([\\s\\S]*?)(><\\/vars>|\\/>)`, 'g')
+    data.replace(varsRx, function (all, items) {
+      items.replace(/(.*?)=(.*?)( |$|\n)/g, function (all, key, val) {
+        if (val.substr(-1) === '"') {
+          vars[key.trim()] = val.trim().slice(1, -1)
+        } else {
+          vars[key.trim()] = val.trim()
+        }
       })
     })
-  }
+
+    let injects = {
+      leftovers: [data]
+    }
+
+    const insertRx = new RegExp(`<insert into="(.*?)">([\\s\\S]*?)</insert>`, 'g')
+    data.replace(insertRx, function (all, where, content) {
+      if (!injects[where]) injects[where] = []
+      injects[where].push(content)
+      injects.leftovers[0] = injects.leftovers[0].replace(all, '')
+    })
+
+    for (let key in injects) {
+      injects[key] = injects[key].join('\n')
+    }
+
+    options = Object.assign(options, vars, injects)
+
+    fs.readFile(path.join(rootDir, (options.template || 'default') + '.ejs'), function (err, data) {
+      if (err) console.log(err)
+      const finalHtml = ejs.render(data.toString(), options)
+
+      callback(null, finalHtml)
+    })
+  })
 }
